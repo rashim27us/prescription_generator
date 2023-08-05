576d3282e74fc8fae8bf4b9ac69d3df62b478623
rashim27us <114280987+rashim27us@users.noreply.github.com>
2023-08-05 16:36:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Prescription Generator</title>
-</head>
-<body>
-    <h1>Prescription Generator</h1>
-    <form action="/submit" method="post" enctype="multipart/form-data">
-        <label for="patient_name">Patient Name:</label>
-        <input type="text" id="patient_name" name="patient_name" required><br>
+import mysql.connector
+from flask import Flask, render_template, request
 
-        <label for="age">Age:</label>
-        <input type="number" id="age" name="age" required><br>
+app = Flask(__name__)
 
-        <label for="gender">Gender:</label>
-        <input type="radio" id="male" name="gender" value="Male" required>
-        <label for="male">Male</label>
-        <input type="radio" id="female" name="gender" value="Female" required>
-        <label for="female">Female</label><br>
+# MySQL database configuration
+app.config['MYSQL_HOST'] = 'your_mysql_host'
+app.config['MYSQL_USER'] = 'your_mysql_username'
+app.config['MYSQL_PASSWORD'] = 'your_mysql_password'
+app.config['MYSQL_DB'] = 'your_mysql_database_name'
 
-        <!-- Add other fields here for patient ID, prescription ID, symptoms, disease, lab report, etc. -->
+# Establish MySQL connection
+connection = mysql.connector.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    database=app.config['MYSQL_DB']
+)
+# You can create a separate config.py file to store these sensitive settings securely.
+# Helper function to get the MySQL connection
+def get_db_connection():
+    return connection
 
-        <label for="lab_report">Lab Report:</label>
-        <input type="file" id="lab_report" name="lab_report"><br>
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
+# Add code here to configure your MySQL connection
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_prescription():
+    if request.method == 'POST':
+        # Aceess form data
+        patient_name = request.form['patient_name']
+        age = request.form['age']
+        gender = request.form['gender']
+        # Add  code  to acces othe form fields
+        lab_report_file = request.files['lab_report']
+        cursor = connection.cursor()
+        return 'Prescription submitted successfully!'
+    return 'Invalid request method'
+    
+if __name__ == '__main__':
+    app.run(debug=True)
